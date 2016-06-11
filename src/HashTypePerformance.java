fcf00c3b321f14d712f792fa7473304a08935a95
vvclin <vvclin@gmail.com>
2016-06-11 17:27:01 +0800

    0611

--- src/HashTypePerformance.java
+++ src/HashTypePerformance.java
@@ -7,68 +7,86 @@ public class HashTypePerformance {
 	
 	public static void main(String[] args) {
 		Random rand = new Random();
-		   int numCalls = 100000;
+		   int numCalls = 100000, numRuns = 5;
 		   int randomNum1;
 		   int tmp;
+		   boolean tmpBol;
 		   long startTime, time;
-		   double callSec;
+		   double callSec = 0;
 		   HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 		   HashSet<Integer> set = new HashSet<Integer>();
 		   int[] array = new int[numCalls];
+		   // fill stuff up
 		   for (int i = 0; i < numCalls; i++) {			   
 			   array[i] = i;
 			   map.put(i, i);
 			   set.add(i);
 		   }
 		   // test for array access
-		   startTime = System.nanoTime();
 		   System.out.println("test for array access: ");
-		   for (int i = 0; i < numCalls; i++) {
-			   randomNum1 = rand.nextInt(numCalls);
-			   tmp = array[randomNum1];
-		   }
-		   time = System.nanoTime() - startTime;
-		   callSec = numCalls  / (double) time;
-		   System.out.println("time elapsed in during calls: " + time);
-		   System.out.println("calls / sec: " + callSec* 1000000000);
+		   callSec = 0;
+		   time = 0;
+		   for (int j = 0; j < numRuns; j++) {
+			   startTime = System.nanoTime();
+			   for (int i = 0; i < numCalls; i++) {
+				   randomNum1 = rand.nextInt(numCalls);
+				   tmp = array[randomNum1];
+			   }
+			   time += System.nanoTime() - startTime;
+			   callSec += numCalls  / (double) time;			   
+		   }		   
+		   System.out.println("average time elapsed during calls: " + time / numRuns);
+		   System.out.println("average calls / sec: " + callSec * 1000000000 / numRuns);
 		   System.out.println();
 
 		   // test for HashMap.get
-		   startTime = System.nanoTime();
 		   System.out.println("test for HashMap.get: ");
-		   for (int i = 0; i < numCalls; i++) {
-			   randomNum1 = rand.nextInt(numCalls);
-			   tmp = map.get(randomNum1);
-		   }
-		   time = System.nanoTime() - startTime;
-		   callSec = numCalls  / (double) time;
-		   System.out.println("time elapsed in during calls: " + time);
-		   System.out.println("calls / sec: " + callSec* 1000000000);
+		   callSec = 0;
+		   time = 0;
+		   for (int j = 0; j < numRuns; j++) {
+			   startTime = System.nanoTime();
+			   for (int i = 0; i < numCalls; i++) {
+				   randomNum1 = rand.nextInt(numCalls);
+				   tmp = map.get(randomNum1);
+			   }
+			   time += System.nanoTime() - startTime;
+			   callSec += numCalls  / (double) time;			   
+		   }		   
+		   System.out.println("average time elapsed during calls: " + time / numRuns);
+		   System.out.println("average calls / sec: " + callSec * 1000000000 / numRuns);
 		   System.out.println();
 		   // test for HashMap.contains
-		   startTime = System.nanoTime();
 		   System.out.println("test for HashMap.contains: ");
-		   for (int i = 0; i < numCalls; i++) {
-			   randomNum1 = rand.nextInt(numCalls);
-			   map.containsKey(randomNum1);
-		   }
-		   time = System.nanoTime() - startTime;
-		   callSec = numCalls  / (double) time;
-		   System.out.println("time elapsed in during calls: " + time);
-		   System.out.println("calls / sec: " + callSec* 1000000000);
+		   callSec = 0;
+		   time = 0;
+		   for (int j = 0; j < numRuns; j++) {
+			   startTime = System.nanoTime();
+			   for (int i = 0; i < numCalls; i++) {
+				   randomNum1 = rand.nextInt(numCalls);
+				   tmpBol = map.containsKey(randomNum1);
+			   }
+			   time += System.nanoTime() - startTime;
+			   callSec += numCalls  / (double) time;			   
+		   }		   
+		   System.out.println("average time elapsed during calls: " + time / numRuns);
+		   System.out.println("average calls / sec: " + callSec * 1000000000 / numRuns);
 		   System.out.println();
 
 		   // test for HashSet.contains
-		   startTime = System.nanoTime();
 		   System.out.println("test for HashSet.contains: ");
-		   for (int i = 0; i < numCalls; i++) {
-			   randomNum1 = rand.nextInt(numCalls);
-			   set.contains(randomNum1);
-		   }
-		   time = System.nanoTime() - startTime;
-		   callSec = numCalls  / (double) time;
-		   System.out.println("time elapsed in during calls: " + time);
-		   System.out.println("calls / sec: " + callSec* 1000000000);
+		   callSec = 0;
+		   time = 0;
+		   for (int j = 0; j < numRuns; j++) {
+			   startTime = System.nanoTime();
+			   for (int i = 0; i < numCalls; i++) {
+				   randomNum1 = rand.nextInt(numCalls);
+				   tmpBol = set.contains(randomNum1);
+			   }
+			   time += System.nanoTime() - startTime;
+			   callSec += numCalls  / (double) time;			   
+		   }		   
+		   System.out.println("average time elapsed during calls: " + time / numRuns);
+		   System.out.println("average calls / sec: " + callSec * 1000000000 / numRuns);
 		   System.out.println();
 		
 	}
